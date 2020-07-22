222442ea0f0e56cf028aacbb0b308d979dc5ae15
Bruno Kucevic <brunokucevic@gmail.com>
2020-07-22 14:59:04 +0200

    Initial Commit

---
+++ Ajmo!ScreenCloneAssignmentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ajmo!ScreenCloneAssignmentApp
+//
+//  Created by Bruno Kucevic on 22/07/2020.
+//  Copyright © 2020 Bruno Kucevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
